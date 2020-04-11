6262edd13fd183937e4dc51cce70f2f3c3924c0e
Martin <m@codedrecordings.com>
2020-04-12 01:03:48 +0200

    More textures, nameplate fixes

--- nameplates.lua
+++ nameplates.lua
@@ -2,58 +2,64 @@ local MTUI = LibStub("AceAddon-3.0"):GetAddon("MTUI")
 
 local options
 
--- local function GetRgb(c)
---     return c.r, c.g, c.b
--- end
+local function GetRgb(c)
+    return c.r, c.g, c.b
+end
 
 local function IsNameplate(unit)
     return type(unit) == "string" and (string.match(unit, "nameplate") == "nameplate" or string.match(unit, "NamePlate") == "NamePlate")
 end
 
-local function UpdateStatusBarTexture(self, ...)
-    if not IsNameplate(self.unit) then return end
-    local texture = MTUI.db.profile.mediaPath..MTUI.db.profile.texture
-    self.healthBar:SetStatusBarTexture(texture)
-    self.castBar:SetStatusBarTexture(texture)
-    if (ClassNameplateManaBarFrame) then
-        ClassNameplateManaBarFrame:SetStatusBarTexture(texture)
+local function UpdateStatusBarColor(frame, ...)
+    if not IsNameplate(frame.unit) then return end
+
+    local r, g, b = UnitSelectionColor(frame.unit, true)
+
+    if UnitReaction(frame.unit, "player") and UnitReaction(frame.unit, "player") > 4 then
+        frame.healthBar:SetStatusBarColor(0, 0.85 * options.unitColorMod, 0)
+    elseif UnitIsTapDenied(frame.unit) and not UnitPlayerControlled(frame.unit) then
+        frame.healthBar:SetStatusBarColor(0.5, 0.5, 0.5)
+    else
+        local threat = UnitThreatSituation("player", frame.unit)
+
+        if threat == 3 then
+            r, g, b = GetRgb(RED_FONT_COLOR)
+        elseif threat == 2 then
+            r, g, b = GetRgb(ORANGE_FONT_COLOR)
+        elseif threat == 1 then
+            r, g, b = GetRgb(YELLOW_FONT_COLOR)
+        end
+        frame.healthBar:SetStatusBarColor(r * options.unitColorMod, g * options.unitColorMod, b * options.unitColorMod)
     end
 end
-
--- local function UpdateNameColor(self, ...)
---     if not IsNameplate(self.unit) then return end
-
---     self.name:SetVertexColor(1, 1, 1)
---     self.name:SetFont(UNIT_NAME_FONT, 8);
--- end
-
--- local function UpdateStatusBarColor(self)
---     if not IsNameplate(self.unit) then return end
---     local r, g, b = UnitSelectionColor(self.unit)
---     local threat = UnitThreatSituation("player", self.unit)
---     if threat == 3 then
---         r, g, b = GetRgb(RED_FONT_COLOR)
---     elseif threat == 2 then
---         r, g, b = GetRgb(ORANGE_FONT_COLOR)
---     elseif threat == 1 then
---         r, g, b = GetRgb(YELLOW_FONT_COLOR)
---     end
---     self.healthBar:SetStatusBarColor(r * options.unitColorMod, g * options.unitColorMod, b * options.unitColorMod)
--- end
 
 function MTUI:InitializePlates()
     options = self.db.profile
 
-    -- hooksecurefunc("CompactUnitFrame_UpdateName", UpdateNameColor)
-    hooksecurefunc("CompactUnitFrame_UpdateName", UpdateStatusBarTexture)
-    -- hooksecurefunc("CompactUnitFrame_UpdateAggroFlash", UpdateStatusBarColor)
-    -- hooksecurefunc("CompactUnitFrame_UpdateHealthColor", UpdateStatusBarColor)
+    hooksecurefunc("CompactUnitFrame_UpdateAggroFlash", UpdateStatusBarColor)
+    hooksecurefunc("CompactUnitFrame_UpdateHealthColor", UpdateStatusBarColor)
+
+    hooksecurefunc("CompactUnitFrame_UpdateName", function(frame, ...)
+        if not IsNameplate(frame.unit) then return end
+
+        local texture = options.mediaPath..options.texture
+        frame.healthBar:SetStatusBarTexture(texture)
+        frame.castBar:SetStatusBarTexture(texture)
+        if (ClassNameplateManaBarFrame) then
+            ClassNameplateManaBarFrame:SetStatusBarTexture(texture)
+        end
+        frame.selectionHighlight:Hide()
+        frame.name:SetVertexColor(1, 1, 1)
+        frame.name:SetFont(UNIT_NAME_FONT, 8);
+    end)
+
     hooksecurefunc("DefaultCompactNamePlateFrameAnchorInternal", function(frame, ...)
         if not IsNameplate(frame.unit) then return end
 
-        frame.healthBar:SetHeight(6)
-        frame.name:SetPoint("BOTTOM", frame.healthBar, "TOP", 0, 1)
-        frame.name:SetFont(UNIT_NAME_FONT, 8)
+        frame.name:SetPoint("BOTTOM", frame.healthBar, "TOP", 0, 2)
+        frame.name:SetFont(UNIT_NAME_FONT, 7)
         frame.castBar.Text:SetFont(UNIT_NAME_FONT, 6)
+        frame.castBar.Text:SetPoint("TOP", frame.castBar, "BOTTOM", 0, 2)
+        frame.healthBar:SetHeight(5)
     end)
 end
